b3a377f6a42dd869a9719960dd0d544f9c30bc61
ayush <ayushkumarsingh97@gmail.com>
2016-07-02 14:16:44 +0530

    final changes made

--- titanic.py
+++ titanic.py
@@ -4,7 +4,11 @@ from sklearn.tree import DecisionTreeClassifier
 from sklearn import tree
 from sklearn.grid_search import GridSearchCV
 from sklearn.pipeline import Pipeline
+import csv as csv
+from itertools import izip
 
+global ids
+ids=[]
 def preprocessing(file_name):
     df = pd.read_csv(file_name,delimiter=',',header=0)
 
@@ -38,9 +42,14 @@ def preprocessing(file_name):
     df = np.array(df)
 
 
+
     return df
 
-
+def passenger_id(file_name):
+    df = pd.read_csv(file_name,delimiter=',',header=0)
+    ids = df["PassengerId"].tolist()
+    # print ids
+    return ids
 
 df = preprocessing("train.csv")
 X_train = df[:,1:]
@@ -49,16 +58,45 @@ Y_train = df[:,0].tolist()
 
 df = preprocessing("test.csv")
 X_test = df
+ids = passenger_id("test.csv")
 
+pipeline=  Pipeline([
+                    ('clf',DecisionTreeClassifier(criterion="entropy"))
+                    ])
 
-print Y_train[0]
-titanic = DecisionTreeClassifier(criterion="entropy")
-titanic.fit(X_train,Y_train)
+parameters={
+    'clf__max_depth':(150,155,160),
+    'clf__min_samples_split':(1,2,3),
+    'clf__min_samples_leaf':(1,2,3)
 
-tree.export_graphviz(titanic,out_file='tree.dot')
-prediction = titanic.predict(X_test)
+}
+# print Y_train[0]
 
+#this is for without GridSearchCV
+# titanic = DecisionTreeClassifier(criterion="entropy")
+# titanic.fit(X_train,Y_train)
 
+grid_search = GridSearchCV(pipeline,parameters,n_jobs=1,verbose=1,scoring='f1')
+
+grid_search.fit(X_train,Y_train)
+
+print "Best score:",grid_search.best_score_
+print "Best parameters set:"
+best_parameters = grid_search.best_estimator_.get_params()
+
+for param_name in sorted(parameters.keys()):
+    print (param_name,best_parameters[param_name])
+
+# tree.export_graphviz(grid_search,out_file='tree.dot')
+
+# prediction = t.predict(X_test)
+prediction = grid_search.predict(X_test).tolist()
 print prediction
 
-# print titanic.score(prediction,Y_test)
+with open("myfirstforest.csv", "wb") as predictions_file:
+    # predictions_file = open("myfirstforest.csv", "wb")
+    open_file_object = csv.writer(predictions_file, delimiter=',')
+    open_file_object.writerow(["PassengerId","Survived"])
+    open_file_object.writerows(zip(ids,prediction))
+    # predictions_file.close()
+    print len(zip(ids,prediction))
